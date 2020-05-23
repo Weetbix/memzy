9e1e0f6bc791ff77d1016f2856a2e26413bd8d7a
John Hannagan <weetbix_@hotmail.com>
2020-05-23 17:39:03 +0200

    Improve card styles and add animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,39 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const flipAnimation = keyframes`
+  0% {
+    transform: scaleX(1.0);
+    background-image: none
+  }
+  50% {
+    transform: scaleX(0);
+    background-image: reset;
+  }
+  0% {
+    transform: scaleX(1.0);
+  }
+`;
+
+const unflipAnimation = keyframes`
+  0% {
+    transform: scaleX(1.0);
+  }
+  50% {
+    transform: scaleX(0);
+    background-image: none;
+  }
+  0% {
+    transform: scaleX(1.0);
+  }
+`;
 
 const Card = styled.button`
   margin: 20px;
   width: 100px;
   height: 100px;
+  border-radius: 100px;
+  border-color: rgba(0, 0, 0, 0.1);
+
   background-image: url(${(props) => {
     const imgUrl = `${process.env.PUBLIC_URL}/cards/${props.type}.svg`;
     return props.flipped ? imgUrl : null;
@@ -12,7 +42,11 @@ const Card = styled.button`
   background-repeat: no-repeat;
   background-position: center;
   box-shadow: 5px 5px 10px 1px rgba(0, 0, 0, 0.3);
-  border-radius: 10px;
+
+  animation-name: ${(props) =>
+    props.flipped ? flipAnimation : unflipAnimation};
+  animation-duration: 0.2s;
+  animation-iteration-count: 1;
 
   :focus {
     outline: 0;
